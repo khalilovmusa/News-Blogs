258e7fc1ba457fbfa34a11afd50050953f163b60
Musa <Musaxlilov29@gmail.com>
2025-02-08 12:09:24 +0400

    First commit

---
+++ src/Components/News.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import Weather from './Weather'
+import Calendar from './Calendar'
+import "./News.css"
+
+const News = () => {
+    return (
+        <div className='news'>
+            <header className='news-header'>
+                <h1 className="logo">News & Blogs</h1>
+                <div className="search-bar">
+                    <form>
+                        <input type="text" placeholder="Search News..." />
+                        <button type="submit">
+                            <i className="fa-solid fa-magnifying-glass"></i>
+                        </button>
+                    </form>
+                </div>
+            </header>
+            <div className="news-content">
+                <div className='navbar'>
+                    <div className="user">User</div>
+                    <nav className='categories'>categories</nav>
+                </div>
+                <div className="news-section">
+                    <div className="headline">Headline</div>
+                    <div className="news-grid">News grid</div>
+                </div>
+                <div className="my-blogs">My Blogs</div>
+                <div className="weather-calendar">
+                    <Weather />
+                    <Calendar />
+                </div>
+            </div>
+            <footer className='news-footer'>Footer</footer>
+        </div>
+    )
+}
+
+export default News
